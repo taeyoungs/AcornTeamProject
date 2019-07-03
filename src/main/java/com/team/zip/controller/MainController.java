f6ad38d32efd6906ef2bcd92e89bf876c7c06953
sowon7912 <spark3766>
2019-07-03 14:36:18 +0900

    7월 3일 오전 lib 오류 수정 후 commit

--- src/main/java/com/team/zip/controller/MainController.java
+++ src/main/java/com/team/zip/controller/MainController.java
@@ -1,4 +1,4 @@
-package team.project.controller;
+package com.team.zip.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

d579fd3f182c2852c59f56bbfd38d3f9b33889ec
sowon7912 <spark3766>
2019-07-04 18:24:00 +0900

    MemberDAO Import 추가 7월4일 최종 오류 수정

--- src/main/java/com/team/zip/service/PhotoService.java
+++ src/main/java/com/team/zip/service/PhotoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.team.zip.model.dao.MemberDAO;
 import com.team.zip.model.dao.PhotoDAO;
 import com.team.zip.model.vo.PhotoVO;
 

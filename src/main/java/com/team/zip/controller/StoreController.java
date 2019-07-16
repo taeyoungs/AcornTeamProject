af460329107c63405a4e9fad7a7482211422e3b9
huruchini <huruchinijbcg@gmail.com>
2019-07-16 18:18:25 +0900

    #190716 변경사항 1. filewrite시에 절대 경로 설정되어있음 2. 스토어 - 랭킹 페이지 jsp,css만 적용되어있음 @ 스토어 controller 만드는 중

--- src/main/java/com/team/zip/controller/StoreController.java
+++ src/main/java/com/team/zip/controller/StoreController.java
@@ -2,13 +2,23 @@ package com.team.zip.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StoreController {
 	@RequestMapping("/store/store.do")
 	public String storeGo() {
 		
-		return "/store/storehome";
+		return "/store/ranks";
+	}
+	
+	@RequestMapping("/store/rank.do")
+	public ModelAndView rankGo() {
+		ModelAndView model = new ModelAndView();
+		
+		
+		model.setViewName("/store/ranks");
+		return model;
 	}
 	
 }
